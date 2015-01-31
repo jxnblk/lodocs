e75e88d132cb0f3973755cf5c3d12d060f8847c2
Brent Jackson <jacksonblack@gmail.com>
2015-01-31 16:33:00 -0500

    Split up modules

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 var _ = require('lodash');
 var fs = require('fs');
 var path = require('path');
-var fm = require('front-matter');
 
 var read = require('./lib/read');
 var extend = require('./lib/extend');
 var md = require('./lib/md');
 var helpers = require('./lib/helpers');
 var include = require('./lib/include');
+var generatePages = require('./lib/generate-pages');
+var formatRoutes = require('./lib/format-routes');
+var parseModules = require('./lib/parse-modules');
+var humanizeName = require('./lib/humanize-name');
+
 
 module.exports = function(data) {
 
   var self = this;
-
-  this.data = function() {
-    return self;
-  };
 
   if (!data.source) {
     console.error('No source provided');
@@ -29,143 +29,23 @@ module.exports = function(data) {
     this[key] = val;
   });
 
-
   this.defaultLayout = read(path.join(this.source, this.layout)) || read(path.join(__dirname, './layouts/default.html'));
 
   this.layout = this.defaultLayout;
 
   this.helpers = this.helpers || {};
-
   _.forIn(helpers, function(val, key) {
     this[key] = val;
   });
-
   _.forIn(this.helpers, function(val, key) {
     this[key] = val;
   });
 
-  // Partials
   this.partials = this.partials || {};
   this.include = include;
-
-  // Extend layouts
   this.extend = extend;
-
-  this.title = this.title || _.capitalize(this.name.replace(/\-/g, ' '));
-
+  this.title = this.title || humanizeName(this.name);
   this.routes = this.routes || {};
-
-
-  // Private functions
- 
-  function capitalize(string) {
-    return _.capitalize(string.replace(/\-/g, ' '));
-  };
-
-  function formatRoutes(routes, root) {
-    var root = root || '';
-    var keys = Object.keys(routes);
-    keys.forEach(function(key) {
-      var route = routes[key];
-      if (route.path) {
-        route.path = root + route.path;
-      } else {
-        route.path = root + '/' + key;
-      }
-      var source = route.source || key;
-      if (source) {
-        if (fs.existsSync('./node_modules' + source)) {
-          var pkg = require(source + '/package.json');
-          route.source = pkg.name;
-          route.title = route.title || capitalize(pkg.name);
-        }
-      }
-      // Set title if manually set in routes object
-      route.title = route.title || capitalize(key);
-      if (route.routes) {
-        formatRoutes(route.routes, root + route.path);
-      }
-    });
-  };
-
-  function getModule(name) {
-    if (!fs.existsSync('./node_modules/' + name)) return false;
-    var module = require(name + '/package.json');
-    var markdown = read('./node_modules/' + name + '/README.md');
-    module.content = md(markdown);
-    return module;
-  };
-
-  function parseModules(modules) {
-    var obj = {};
-    modules.forEach(function(name) {
-      obj[name] = getModule(name);
-    });
-    return obj;
-  };
-
-  function generatePages(routes) {
-
-    //var root = root || '';
-    var keys = Object.keys(routes);
-
-    keys.forEach(function(key) {
-      var route = routes[key];
-      var dest = this.dest + route.path;
-      var content = read(path.join(self.source + route.path, './index.html'));
-      //var pageData = _.cloneDeep(data);
-      var pageData = self.data();
-      // Check for markdown file
-      if (!content) {
-        console.log('checking for markdown');
-        var src  = read(path.join(self.source + route.path, './index.md'));
-        var matter = fm(src);
-        _.assign(pageData, matter.attributes);
-        content = md(matter.body);
-      }
-      var source = route.source || key;
-      // Check for parsed module
-      if (typeof modules !== 'undefined' && !content) {
-        var module = modules[source] || false;
-        if (module) {
-          content = modules[source].content;
-          pageData.page = module;
-        }
-      }
-      // Check for node module
-      if (!content && fs.existsSync('./node_modules/' + source)) {
-        pageData.page = require(source + '/package.json');
-        var markdown = read('./node_modules/' + source + '/README.md');
-        content = md(markdown);
-      }
-      if (!content) {
-        console.error('No content found for ' + route.title);
-        route.disabled = true;
-      } else {
-        pageData.page = pageData.page || {};
-        pageData.page.title = route.title;
-        //console.log('pageData', pageData);
-        pageData.content = _.template(content)(pageData);
-        //console.log('content', pageData.content);
-        var template = _.template(self.layout);
-        var html = template(pageData);
-        if (!fs.existsSync(dest)) {
-          console.log(dest);
-          fs.mkdirSync(dest);
-        }
-        console.log('write file', path.join(dest, './index.html'));
-        fs.writeFileSync(path.join(dest, './index.html'), html);
-      }
-      // Reset layout
-      self.layout = self.defaultLayout;
-      var subroutes = route.routes || false;
-      if (subroutes) {
-        generatePages(subroutes);
-      }
-    });
-
-  };
-
 
   // Format and render
   formatRoutes(this.routes);
--- test/index.js
+++ test/index.js
@@ -5,6 +5,9 @@ var lodocs = require('..');
 
 var data = require('../package.json');
 data.routes = require('./routes.json');
+data.modules = [
+  'flex-object'
+];
 
 data.source = path.join(__dirname, './src');
 data.dest = path.join(__dirname, './dest');
@@ -12,12 +15,7 @@ data.dest = path.join(__dirname, './dest');
 data.layout = './layouts/base.html';
 data.stylesheet = 'http://d2v52k3cl9vedd.cloudfront.net/blk/0.0.14/blk.min.css';
 
-//data.partials = {};
-//data.partials.footer = fs.readFileSync(path.join(__dirname, './partials/footer.html'), 'utf8');
-//data.partials.nav = fs.readFileSync(path.join(__dirname, './partials/nav.html'), 'utf8');
-
 data.partials = {};
-//data.helpers.sidebar = fs.readFileSync(path.join(__dirname, './partials/sidebar.html'), 'utf8');
 data.partials.footer = fs.readFileSync(path.join(__dirname, './partials/footer.html'), 'utf8');
 data.partials.nav = fs.readFileSync(path.join(__dirname, './partials/nav.html'), 'utf8');
 
